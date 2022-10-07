2dc66297befea3788e253ed7052d53556d9e46f1
Matthew Hall <matthew@quickbeam.me.uk>
2022-10-07 14:53:44 +0100

    Improve styling and styling code
    * "Dead" todos (cancelled or done) are now faded out
    * Introduce functions for deciding how each field should be styled

--- src/shared.zig
+++ src/shared.zig
@@ -4,6 +4,8 @@ const Db = @import("Db.zig");
 const Style = @import("Style.zig");
 const Statements = @import("Statements.zig");
 
+const dead_style = Style{ .foreground = Style.grey, .faint = true };
+
 pub const TodoState = enum {
     in_progress,
     in_review,
@@ -21,6 +23,25 @@ pub const TodoState = enum {
     pub fn readField(_: std.mem.Allocator, value: BaseType) !TodoState {
         return std.meta.intToEnum(TodoState, value);
     }
+
+    pub fn dead(self: TodoState) bool {
+        return switch (self) {
+            .done, .cancelled => true,
+            else => false,
+        };
+    }
+
+    pub fn style(self: TodoState, selected: bool) Style {
+        _ = selected;
+        return switch (self) {
+            .in_progress => .{ .foreground = Style.blue },
+            .in_review => .{ .foreground = Style.pink },
+            .todo => .{ .foreground = Style.green },
+            .blocked => .{ .foreground = Style.red },
+            .done => dead_style,
+            .cancelled => dead_style,
+        };
+    }
 };
 
 pub const Todo = struct {
@@ -32,6 +53,38 @@ pub const Todo = struct {
     timed: bool,
 
     const Self = @This();
+
+    pub fn title_style(self: *const Self, selected: bool) Style {
+        _ = selected;
+
+        if (self.state.dead()) return dead_style;
+
+        return .{ .foreground = Style.pink };
+    }
+
+    pub fn id_style(self: *const Self, selected: bool) Style {
+        _ = selected;
+
+        if (self.state.dead()) return dead_style;
+
+        return .{ .bold = true };
+    }
+
+    pub fn priority_style(self: *const Self, selected: bool) Style {
+        _ = selected;
+
+        if (self.state.dead()) return dead_style;
+
+        return .{};
+    }
+
+    pub fn tag_style(self: *const Self, selected: bool) Style {
+        _ = selected;
+
+        if (self.state.dead()) return dead_style;
+
+        return .{ .faint = true };
+    }
 
     pub fn write(self: *const Self, allocator: std.mem.Allocator, writer: anytype, cols: usize) !void {
         var state = try std.ascii.allocUpperString(allocator, std.meta.tagName(self.state));
@@ -62,14 +115,14 @@ pub const Todo = struct {
         var al = std.ArrayList(u8).init(allocator);
         defer al.deinit();
 
-        try (Style{ .bold = true }).print(writer, "{} ", .{self.id});
+        try (self.id_style(false)).print(writer, "{} ", .{self.id});
         if (self.timed) try writer.writeAll("⏰ ");
-        try (Style{ .foreground = Style.green }).print(writer, "{s} ", .{state});
-        try (Style{}).print(writer, "[P-{d}] ", .{self.priority});
-        try (Style{ .foreground = Style.pink }).print(writer, "{s:^[1]}", .{ self.title, title_space });
+        try (self.state.style(false)).print(writer, "{s} ", .{state});
+        try (self.priority_style(false)).print(writer, "[P-{d}] ", .{self.priority});
+        try (self.title_style(false)).print(writer, "{s:^[1]}", .{ self.title, title_space });
 
         if (self.tags.len != 0) {
-            try (Style{ .faint = true }).print(writer, " :{s}:", .{self.tags});
+            try (self.tag_style(false)).print(writer, " :{s}:", .{self.tags});
         } else {
             try al.append('\n');
         }
